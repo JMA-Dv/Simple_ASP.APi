82cd0e5bbba0a013f3f2aba65046125b418393d3
MataLaMaquina69 <jesus@dessauSoftware.com>
2021-06-29 23:50:43 -0500

    added code base for

---
+++ CoreCode.API/Controllers/CampsController.cs
@@ -0,0 +1,41 @@
+﻿using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCode.API.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class CampsController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public async  Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllCampsAsync();
+                return Ok(results);
+
+            }
+            catch (Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError,"Database failide" + ex);
+                
+            }
+            
+            
+        }
+    }
+}
